8d51962c229e33a47f2e57d985e9a790b33144a1
Glitch (hushed-sly-truck) <none>
2020-08-17 12:37:52 +0000

    💽🎥 Checkpoint ./bot.js:12985442/5119

--- bot.js
+++ bot.js
@@ -80,6 +80,46 @@ const help = `**
          
 **`;
 // كههربا (حسن ياسر)
+//embed
+
+//embed
+clien.on("message", message => {
+if (message.author.bot) return;
+
+if (!message.content.startsWith(prefix)) return;
+
+let command = message.content.split(" ")[0];
+command = command.slice(prefix.length);
+
+let args = message.content.split(" ").slice(1);
+
+// +say
+if (command === "say") {
+if (!message.channel.guild)
+return message.channel
+.send("ببورە ئەم ئەمرە تەنها بۆ سێرفەرە")
+.then(m => m.delete(5000));
+if (!message.member.hasPermission("ADMINISTRATOR"))
+return message.channel.send("ببورە ئەم دەسەڵاتەت نیە ADMINISTRATOR");
+message.delete();
+message.channel.sendMessage(args.join(" "));
+} 
+
+if (command == "embed") {
+if (!message.channel.guild)
+return message.channel
+.send("ببورە ئەم ئەمرە تەنها بۆ سێرفەرە")
+.then(m => m.delete(5000));
+if (!message.member.hasPermission("MANAGE_MESSAGES"))
+return message.channel.send("ببورە ئەم دەسەڵاتەت نیە MANAGE_MESSAGES");
+let say = new Discord.RichEmbed()
+.setDescription(args.join(" "))
+.setColor(0x23b2d6);
+message.channel.sendEmbed(say);
+message.delete();
+}
+});
+
 
 const err = `** \`\`\`  [ERORR] : لازم تسوي رتبة بـ اسم  \`\`\`
          \`\` Role.Kahrbaa \`\` **`;
