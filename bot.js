088d6e5db385f8c319ef331b75e096fbb2dc0d43
Glitch (hushed-sly-truck) <none>
2020-08-17 13:00:46 +0000

    🔑🛬 Checkpoint ./bot.js:12985442/2926

--- bot.js
+++ bot.js
@@ -81,7 +81,7 @@ const help = `**
 **`;
 // كههربا (حسن ياسر)
 //embed
-
+//embed
 client.on("message", message => {
 if (message.author.bot) return;
 
@@ -103,6 +103,25 @@ return message.channel.send("ببورە ئەم دەسەڵاتەت نیە ADMINIS
 message.delete();
 message.channel.sendMessage(args.join(" "));
 } 
+
+if (command == "embed") {
+if (!message.channel.guild)
+return message.channel
+.send("ببورە ئەم ئەمرە تەنها بۆ سێرفەرە")
+.then(m => m.delete(5000));
+if (!message.member.hasPermission("MANAGE_MESSAGES"))
+return message.channel.send("ببورە ئەم دەسەڵاتەت نیە MANAGE_MESSAGES");
+let say = new Discord.RichEmbed()
+.setDescription(args.join(" "))
+.setColor(0x23b2d6);
+message.channel.sendEmbed(say);
+message.delete();
+}
+});
+
+
+
+
 
 const err = `** \`\`\`  [ERORR] : لازم تسوي رتبة بـ اسم  \`\`\`
          \`\` Role.Kahrbaa \`\` **`;
