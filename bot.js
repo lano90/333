de93fb2a59205af9f7e534c6434ab62cd38d186a
Glitch (hama-l) <none>
2020-08-29 22:05:44 +0000

    Rewound to commit c0f9be32734719f31f5873e3db26758acc6c8df9 + 0 edits

--- bot.js
+++ bot.js
@@ -80,6 +80,47 @@ const help = `**
          
 **`;
 // كههربا (حسن ياسر)
+//embed
+//embed
+client6.on("message", message => {
+if (message.author.bot) return;
+
+ let prefix = ">"
+let command = message.content.split(" ")[0];
+command = command.slice(prefix.length);
+
+let args = message.content.split(" ").slice(1);
+
+// +say
+if (command === "say") {
+if (!message.channel.guild)
+return message.channel
+.send("ببورە ئەم ئەمرە تەنها بۆ سێرفەرە")
+.then(m => m.delete(5000));
+if (!message.member.hasPermission("ADMINISTRATOR"))
+return message.channel.send("ببورە ئەم دەسەڵاتەت نیە ADMINISTRATOR");
+message.delete();
+message.channel.sendMessage(args.join(" "));
+} 
+
+if (command == "embed") {
+if (!message.channel.guild)
+return message.channel
+.send("ببورە ئەم ئەمرە تەنها بۆ سێرفەرە")
+.then(m => m.delete(5000));
+if (!message.member.hasPermission("MANAGE_MESSAGES"))
+return message.channel.send("ببورە ئەم دەسەڵاتەت نیە MANAGE_MESSAGES");
+let say = new Discord.RichEmbed()
+.setDescription(args.join(" "))
+.setColor(0x23b2d6);
+message.channel.sendEmbed(say);
+message.delete();
+}
+});
+
+
+
+
 
 const err = `** \`\`\`  [ERORR] : لازم تسوي رتبة بـ اسم  \`\`\`
          \`\` Role.Kahrbaa \`\` **`;
@@ -3365,7 +3406,7 @@ client3.on("message", message => {
       .slice(1)
       .join(" ");
     if (!dinfo.owner.includes(message.author.id)) return;
-    if (message.content.startsWith(prefix + "randomava")) {
+    if (message.content.startsWith(prefix + "random")) {
       client3.user.setAvatar("https://media.discordapp.net/attachments/654999835547795486/668881880321359882/image6.jpg?width=611&height=599");
       message.channel.send(`**»وێـنـەی  ئـەڪـاونـتـەڪـان گــۆڕا**`);
     }
@@ -4164,7 +4205,7 @@ client20.on("message", message => {
 // جمــيع الحقوق محفوظة لدي "Kahrbaa"
 
 client.login("");
-client2.login("NzQ0ODY1MjEzNDk2Njg4NzMx.XzpdhA.sJRl9y4W28fFxhvC84zEyYryI9g");
+client2.login("");
 client3.login(process.env.KahTOKEN3); // جمــيع الحقوق محفوظة لدي "Kahrbaa"
 client4.login(process.env.KahTOKEN4);
 client5.login(process.env.KahTOKEN5);
