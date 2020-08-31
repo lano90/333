a365b3d31f1565f340219a93db3b37562a04c24a
Glitch (hama-l) <none>
2020-08-31 21:52:44 +0000

    🐷🎎 Checkpoint ./bot.js:12985442/58

--- bot.js
+++ bot.js
@@ -4195,7 +4195,7 @@ client20.on("message", message => {
 // جمــيع الحقوق محفوظة لدي "Kahrbaa"
 
 client.login("");
-client2.login("zQ0ODY1MjEzNDk2Njg4NzMx.XzpdhA.sJRl9y4W28fFxhvC84zEyYryI9g");
+client2.login("");
 client3.login(process.env.KahTOKEN3); // جمــيع الحقوق محفوظة لدي "Kahrbaa"
 client4.login(process.env.KahTOKEN4);
 client5.login(process.env.KahTOKEN5);
